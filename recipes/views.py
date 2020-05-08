e22b0a2f075f016926c513cf8132a8a7958f61e1
pmundzik <p.mundzik@gmail.com>
2020-05-08 19:10:11 +0200

    Simplify meals templates - all in one

--- recipes/views.py
+++ recipes/views.py
@@ -10,37 +10,58 @@ def index(request):
 
 def all_recipes(request):
     recipes = Recipe.objects.all()
-    return render(request, 'all_recipes.html', {'recipes': recipes})
+    if recipes:
+        return render(request, 'recipe_card.html', {'recipes': recipes})
+    else:
+        return render(request, 'no_recipes.html')
 
 
 def all_breakfasts(request):
-    breakfasts = Recipe.objects.all()
-    return render(request, 'all_breakfasts.html', {'recipes': breakfasts})
+    breakfasts = Recipe.objects.filter(type_of_meal="breakfast")
+    if breakfasts:
+        return render(request, 'recipe_card.html', {'recipes': breakfasts})
+    else:
+        return render(request, 'no_recipes.html')
 
 
 def all_lunches(request):
-    lunches = Recipe.objects.all()
-    return render(request, 'all_lunches.html', {'recipes': lunches})
+    lunches = Recipe.objects.filter(type_of_meal="lunch")
+    if lunches:
+        return render(request, 'recipe_card.html', {'recipes': lunches})
+    else:
+        return render(request, 'no_recipes.html')
 
 
 def all_salads(request):
-    salads = Recipe.objects.all()
-    return render(request, 'all_salads.html', {'recipes': salads})
+    salads = Recipe.objects.filter(type_of_meal="salad")
+    if salads:
+        return render(request, 'recipe_card.html', {'recipes': salads})
+    else:
+        return render(request, 'no_recipes.html')
 
 
 def all_dinners(request):
-    dinners = Recipe.objects.all()
-    return render(request, 'all_dinners.html', {'recipes': dinners})
+    dinners = Recipe.objects.filter(type_of_meal="dinner")
+    if dinners:
+        return render(request, 'recipe_card.html', {'recipes': dinners})
+    else:
+        return render(request, 'no_recipes.html')
 
 
 def all_soups(request):
-    soups = Recipe.objects.all()
-    return render(request, 'all_soups.html', {'recipes': soups})
+    soups = Recipe.objects.filter(type_of_meal="soup")
+    if soups:
+        return render(request, 'recipe_card.html', {'recipes': soups})
+    else:
+        return render(request, 'no_recipes.html')
 
 
 def all_desserts(request):
-    desserts = Recipe.objects.all()
-    return render(request, 'all_desserts.html', {'recipes': desserts})
+    desserts = Recipe.objects.filter(type_of_meal="dessert")
+    if desserts:
+        return render(request, 'recipe_card.html', {'recipes': desserts})
+    else:
+        return render(request, 'no_recipes.html')
 
 
 def favourites(request):
